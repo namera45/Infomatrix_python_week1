9305fd2bcadcf0fe0b646bd88625606cea4e4970
aagam mehta <aagammehta327@gmail.com>
2024-06-09 15:21:35 +0530

    Update q2.py

---
+++ q2.py
@@ -0,0 +1,30 @@
+
+#q2.i
+'''
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+reversed_list = my_list[::-1]
+result = reversed_list[1::2]
+print(result)
+'''
+#q2.ii
+'''
+my_list =['d','e','l','a','e','d']
+my_list.insert(4,'y')
+
+print(my_list)
+'''
+#q2.iii
+'''
+my_list =['d','e','l','a','e','d']
+my_list.insert(4,'y')
+my_list1=['','i','t','','i','s']
+my_list.extend(my_list1)
+print(my_list)
+'''
+#q2.v
+'''
+my_list =['d','e','l','a','e','d']
+my_list=my_list[:-1]
+
+print(my_list)
+'''
