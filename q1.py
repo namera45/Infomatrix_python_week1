80b0a9a449923e65dcc3d8a26f42e87dd4bea928
aagam mehta <aagammehta327@gmail.com>
2024-06-09 12:52:28 +0530

    q1.done

---
+++ q1.py
@@ -0,0 +1,22 @@
+#q1.i
+'''
+my_list = [1, 2, 3, 4, 5]
+count = len(my_list)
+print("Number of elements in the list:", count)
+
+'''
+#q1.ii
+'''
+my_list = [1, 2, 0,3, 4, 5,-6,7,-8,9,0,-11,12,0,13,14,-15,16,-17]
+my_list1=[i for i in my_list if i>0]
+my_list2=[i for i in my_list if i<0]
+my_list3=[i for i in my_list if i==0]
+print("negetive numbers are ",my_list2)
+print("positive numbers are",my_list1)
+print("zero's are ", my_list3)
+'''
+#q1.iii
+'''
+sample_dict = {'a': 100, 'b': 200, 'c': 300, 'd':400,'e':500 }
+print(sample_dict)
+'''
